89863b4c47742c8f5819b80146eeeaffe368216b
Elena Glassman <elena.glassman@gmail.com>
2025-06-18 12:42:17 -0400

    update __init__.py

--- src/gpt_scientist/__init__.py
+++ src/gpt_scientist/__init__.py
@@ -1,7 +1,7 @@
-"""Top-level package for GPT Scientist."""
+"""Top-level package for GPT Scientist Scholar."""
 
-__author__ = """Nadia Polikarpova"""
-__email__ = 'nadia.polikarpova@gmail.com'
-__version__ = '0.1.0'
+__author__ = """Elena Glassman"""
+__email__ = 'elena.glassman@gmail.com'
+__version__ = '0.0.1'
 
 from .gpt_scientist import Scientist
